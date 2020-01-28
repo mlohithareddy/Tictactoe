932173003308b6f787b791c101781a46b15f6d5d
LohithaM <lohitham@INLohithaMs-MacBook-Air.local>
2020-01-28 17:23:34 +0530

    tictactoe_names_modified

--- TicTacToe.java
+++ TicTacToe.java
@@ -6,83 +6,82 @@ import java.awt.event.*;
 
 import javax.swing.*;
 public class TicTacToe implements ActionListener{
-	int chance=0;
-	JFrame frame = new JFrame("GridLayout Demo"); //creating frame
-	//build labels
-	JButton btn[]=new JButton[9];
+	int chance_for_player=0;
+	JFrame frame = new JFrame("GridLayout Demo");
+	JButton button[]=new JButton[9];
 	public  TicTacToe()
 	{
-		for(int i=0;i<9;i++)
+		for(int button_no=0;button_no<9;button_no++)
 		{
-			btn[i]=new JButton(" ");
-		    btn[i].setForeground(Color.RED);
-		    btn[i].setBorder(BorderFactory.createLineBorder(Color.black, 5));
-		    btn[i].addActionListener(this);
+			button[button_no]=new JButton(" ");
+			button[button_no].setForeground(Color.RED);
+		    button[button_no].setBorder(BorderFactory.createLineBorder(Color.black, 5));
+		    button[button_no].addActionListener(this);
 		}
 		JPanel panel = new JPanel(new GridLayout(3, 3, 5, 5));
-		for(int i=0;i<9;i++)
-			panel.add(btn[i]);
+		for(int button_no=0;button_no<9;button_no++)
+			panel.add(button[button_no]);
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 	    frame.setSize(300, 150); 
 	    frame.getContentPane().add(panel); 
 	    frame.setVisible(true); 
 	}
+	
 	//static method cannot hide action listener
 	public void actionPerformed(ActionEvent e)
 	{
-			//player 1 will always take X
-			JButton btn=(JButton)e.getSource();
-			if(chance%2==0 && (btn.getText()!="X" && btn.getText()!="O"))
+			JButton source=(JButton)e.getSource();
+			boolean is_occupied=(source.getText()!="X" && source.getText()!="O");
+			if(chance_for_player%2==0 && is_occupied)
 			{
-				btn.setText("X");
-				chance+=1;
+				source.setText("X");
+				chance_for_player+=1;
 			}
-			else if(chance%2==1 && (btn.getText()!="X" && btn.getText()!="O"))
+			else if(chance_for_player%2==1 && is_occupied)
 			{
-				btn.setText("O");
-				chance+=1;
+				source.setText("O");
+				chance_for_player+=1;
 			}
-			if(chance==9)
+			if(chance_for_player==9)
 			{
 				JOptionPane.showConfirmDialog(null, "Game Over.");
 				close_window();
 			} 
 			if(check() == true)
             {
-				String s="Game Over, player "+(chance%2)+" Won!";
+				String s="Game Over, player "+(chance_for_player%2)+" Won!";
                 JOptionPane.showConfirmDialog(null,s);
                 close_window();
             }
-			 
-			
 	}
+	
+	
 	public void close_window()
 	{
 		frame.setVisible(false);
         frame.dispose();
 	}
+	
+	
 	public boolean check()
 	{
-		//row
-		for (int i=0; i<3; i+=3) 
+		for (int button_no=0; button_no<3; button_no+=3) 
 	    { 
-	        if (btn[i+0].getText().equals(btn[i+1].getText()) && 
-	            btn[i+1].getText().equals(btn[i+2].getText()) &&  
-	            !(btn[i+0].getText().equals(" "))) 
+	        if (button[button_no+0].getText().equals(button[button_no+1].getText()) && 
+	            button[button_no+1].getText().equals(button[button_no+2].getText()) &&  
+	            !(button[button_no+0].getText().equals(" "))) 
 	            return (true); 
 	    }
-		//col
-		for (int i=0; i<3; i+=3) 
+		for (int button_no=0; button_no<3; button_no+=3) 
 	    { 
-	        if (btn[i+0].getText().equals(btn[i+3].getText()) && 
-	            btn[i+3].getText().equals(btn[i+6].getText()) &&  
-	            !(btn[i+0].getText().equals(" "))) 
+	        if (button[button_no+0].getText().equals(button[button_no+3].getText()) && 
+	        		button[button_no+3].getText().equals(button[button_no+6].getText()) &&  
+	            !(button[button_no+0].getText().equals(" "))) 
 	            return (true); 
 	    } 
-	    //dia
-		if(btn[0].getText().equals(btn[4].getText()) && btn[4].getText().equals(btn[8].getText()) && (!btn[4].getText().contentEquals(" ")))
+		if(button[0].getText().equals(button[4].getText()) && button[4].getText().equals(button[8].getText()) && (!button[4].getText().contentEquals(" ")))
 			return true;
-		if(btn[2].getText().equals(btn[4].getText()) && btn[4].getText().equals(btn[6].getText()) && (!btn[4].getText().contentEquals(" ")))
+		if(button[2].getText().equals(button[4].getText()) && button[4].getText().equals(button[6].getText()) && (!button[4].getText().contentEquals(" ")))
 			return true;
 	    return(false); 
 	}
